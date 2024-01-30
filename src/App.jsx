dd70006d74dcce3b55866e6ed5f7d25e603a9349
AGUACATE2304 <alan.castillo@mataro.epiaedu.cat>
2024-01-30 18:47:14 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const name = "Alan Castillo Esquina";
+  return (
+    <>
+      <h1>{`Hello ${name}`}</h1>
+    </>
+  );
+}
+
+export default App;
